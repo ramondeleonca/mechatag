d9f8271669aff3aea2b2158677a9bfdbe805a695
Ramón de León <ramondeleonca@gmail.com>
2025-06-10 12:28:41 -0600

    Init

---
+++ main.py
@@ -0,0 +1,31 @@
+import cv2
+import flask
+from flask_cors import CORS
+
+# Set up camera
+camera = cv2.VideoCapture(0)
+
+# Set up server
+app = flask.Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/video')
+def video_feed():
+    def generate_frames():
+        while True:
+            success, frame = camera.read()
+            if not success:
+                continue
+            else:
+                # Encode frame as JPEG
+                ret, buffer = cv2.imencode('.jpg', frame)
+                frame = buffer.tobytes()
+                
+                # Yield the frame in the correct format for streaming
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                
+    return flask.Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# Run the server
+app.run("0.0.0.0", port=8000)
