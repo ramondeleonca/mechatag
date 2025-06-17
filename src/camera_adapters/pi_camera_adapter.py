13c55102bcc382986c331094bd278e050d2b5190
Ramón de León <ramondeleonca@gmail.com>
2025-06-17 14:54:55 -0600

    Fixed pi camera adapter

--- src/camera_adapters/pi_camera_adapter.py
+++ src/camera_adapters/pi_camera_adapter.py
@@ -8,6 +8,7 @@ class PiCameraAdapter(CameraAdapter):
     def __init__(self):
         self.camera = Picamera2()
         self.camera.configure(self.camera.create_video_configuration(main={"format": "XRGB8888", "size": (640, 480)}))
+        self.camera.start()
 
     def get_frame(self):
         return self.camera.capture_array()
